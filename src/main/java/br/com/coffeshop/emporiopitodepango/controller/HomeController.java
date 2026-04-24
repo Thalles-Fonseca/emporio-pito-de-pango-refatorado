6560a3dd3134a638dcd755be8795074dfd6c8eec
Thalles Fonseca <thallesfonseca17@gmail.com>
2026-04-24 12:21:16 -0300

    Integração das paginas HTML com Thymeleaf

--- src/main/java/br/com/coffeshop/emporiopitodepango/controller/HomeController.java
+++ src/main/java/br/com/coffeshop/emporiopitodepango/controller/HomeController.java
@@ -3,6 +3,8 @@ package br.com.coffeshop.emporiopitodepango.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
 @Controller
 public class HomeController {
 
